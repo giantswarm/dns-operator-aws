fc3ab707bdf880b36cf869be5ddf40a09ac41f45
Jose Armesto <github@armesto.net>
2022-10-04 15:52:57 +0200

    Don't apply RBAC for PSPs if PSPs are not deployed (#142)
    * Don't apply RBAC for PSPs if PSPs are not deployed
    
    * Fix linting issues

--- pkg/record/recorder.go
+++ pkg/record/recorder.go
@@ -1,21 +1,24 @@
 package record
 
 import (
-	"strings"
 	"sync"
 
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 )
 
 var (
+	caser           cases.Caser
 	initOnce        sync.Once
 	defaultRecorder record.EventRecorder
 )
 
 func init() {
 	defaultRecorder = new(record.FakeRecorder)
+	caser = cases.Title(language.English)
 }
 
 // InitFromRecorder initializes the global default recorder. It can only be called once.
@@ -28,20 +31,20 @@ func InitFromRecorder(recorder record.EventRecorder) {
 
 // Event constructs an event from the given information and puts it in the queue for sending.
 func Event(object runtime.Object, reason, message string) {
-	defaultRecorder.Event(object, corev1.EventTypeNormal, strings.Title(reason), message)
+	defaultRecorder.Event(object, corev1.EventTypeNormal, caser.String(reason), message)
 }
 
 // Eventf is just like Event, but with Sprintf for the message field.
 func Eventf(object runtime.Object, reason, message string, args ...interface{}) {
-	defaultRecorder.Eventf(object, corev1.EventTypeNormal, strings.Title(reason), message, args...)
+	defaultRecorder.Eventf(object, corev1.EventTypeNormal, caser.String(reason), message, args...)
 }
 
 // Event constructs a warning event from the given information and puts it in the queue for sending.
 func Warn(object runtime.Object, reason, message string) {
-	defaultRecorder.Event(object, corev1.EventTypeWarning, strings.Title(reason), message)
+	defaultRecorder.Event(object, corev1.EventTypeWarning, caser.String(reason), message)
 }
 
 // Eventf is just like Event, but with Sprintf for the message field.
 func Warnf(object runtime.Object, reason, message string, args ...interface{}) {
-	defaultRecorder.Eventf(object, corev1.EventTypeWarning, strings.Title(reason), message, args...)
+	defaultRecorder.Eventf(object, corev1.EventTypeWarning, caser.String(reason), message, args...)
 }
